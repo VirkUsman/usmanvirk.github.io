557d1ec14a0502880911e44bacc8a680023141a0
Usman Virk <usmanvirk11@gmail.com>
2023-09-13 13:32:24 +0200

    Logo Change

--- Components/Socials.js
+++ Components/Socials.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-import {RiYoutubeLine, RiFacebookLine, RiInstagramLine, RiDribbbleLine, RiBehanceLine, RiPinterestLine, RiTwitterLine} from "react-icons/ri"
+import {RiTwitterXLine, RiFacebookLine, RiInstagramLine, RiDribbbleLine, RiBehanceLine, RiPinterestLine, RiTwitterLine} from "react-icons/ri"
 
 const Socials = () => {
     return (
         <div className="flex items-center gap-x-5 text-lg">
             <Link href={''} className=" hover:text-space transition-all duration-300">
-            <RiYoutubeLine />
+            <RiTwitterXLine />
             </Link>
             <Link href={''} className=" hover:text-space transition-all duration-300">
             <RiFacebookLine />
