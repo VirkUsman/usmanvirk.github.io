8636fe1e62d1eaf27fa6bf1d64c9c0bc0a37c3e7
Usman Virk <usmanvirk11@gmail.com>
2023-10-17 15:59:48 +0200

    CV uploaded

--- Components/Socials.js
+++ Components/Socials.js
@@ -16,47 +16,47 @@ const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
       <Link
-        href={"https://github.com/RiaVirk"}
+        href={"https://github.com/VirkUsman"}
         className=" hover:text-space transition-all duration-300"
       >
         <RiGithubLine />
       </Link>
       <Link
-        href={"https://www.xing.com/profile/Maria_Virk"}
+        href={"https://www.xing.com/profile/Usman_Virk023532"}
         className=" hover:text-space transition-all duration-300"
       >
         <RiXingLine />
       </Link>
       <Link
-        href={"https://www.linkedin.com/in/maria-virk"}
+        href={"https://www.linkedin.com/in/usman-virk"}
         className=" hover:text-space transition-all duration-300"
       >
         <RiLinkedinLine />
       </Link>
-      <Link
+      {/* <Link
         href={"https://www.youtube.com/channel/UCmetTl7zsZxADEgfTipAEwQ"}
         className=" hover:text-space transition-all duration-300"
       >
         <RiYoutubeLine />
-      </Link>
-      <Link
+      </Link> */}
+      {/* <Link
         href={
-          "https://www.facebook.com/people/Maria-Virk/pfbid0KZVhkR9HLgD61BG9hCYG7RvxipNHpD6Bq1u4LeB8i6xEpPxA15evzcPEKWqaiecrl/"
+          "https://www.facebook.com/people/"
         }
         className=" hover:text-space transition-all duration-300"
       >
         <RiFacebookLine />
-      </Link>
+      </Link> */}
       <Link
-        href={"https://twitter.com/RiaVirk"}
+        href={"https://twitter.com/VirkUsman9428"}
         className=" hover:text-space transition-all duration-300"
       >
         <RiTwitterXLine />
       </Link>
 
       <a
-        href="/MariaVirk_CV.pdf"
-        download="MariaVirk_CV"
+        href="/CV_UV.pdf"
+        download="UsmanVirk_CV"
         className=" rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-500 flex items-center justify-center overflow-hidden hover:border-space group"
       >
         <span className="text-space group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-700">
